7d6827a7447e41c5002c0ffda0fc8ee7bdc68173
Thomas Hepner <hepner.thomas@gmail.com>
2022-08-12 14:08:23 -0700

    feat: add movies model to server.

---
+++ server/models/movies.js
@@ -0,0 +1,32 @@
+var db = require('./database');
+
+module.exports = {
+
+  // get all movies
+  get: function(callback) {
+    var queryString = 'SELECT * FROM movies';
+    db.query(queryString, (err, res) => {
+      callback(err, results);
+    });
+  },
+
+  // add a movie to the list
+  add: function(params, callback) {
+    var queryString = 'INSERT INTO movies VALUES(NULL, ?, "To Watch")';
+    db.query(queryString, params, function(err, results) => {
+      callback(err, results);
+    });
+  },
+
+  // change a movie's status
+  update: function(params, callback) {
+    var queryString = 'UPDATE movies \
+                      SET status = IF(status = "Watched", "To Watch", "Watched") \
+                      WHERE id = ?';
+    db.query(queryString, params, function(err, results) => {
+      callback(err, results);
+    });
+  }
+
+
+};
