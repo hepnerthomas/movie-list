adffbdfc9029ca5b87e349021da6342c1cb83003
Thomas Hepner <hepner.thomas@gmail.com>
2022-08-12 11:40:34 -0700

    feat: add movies database schema.

---
+++ server/schema.sql
@@ -0,0 +1,30 @@
+/*  Execute this file from the command line by typing:
+ *    mysql -u root < server/schema.sql -p
+ *  to create the database and the tables.*/
+
+/* (Instructions for WSL) Check if MySQL installed and start server by typing:
+*   which mysql
+*   mysql --version
+*   sudo service mysql start
+*   sudo service mysql status
+*/
+
+/* Confirm database exists:
+*  Login to database: mysql -u root -p
+*  SHOW DATABASES;
+*  USE movies;
+*  SHOW TABLES;
+*  SELECT * FROM movies;
+*  DESCRIBE movies;
+*/
+
+DROP DATABASE IF EXISTS movies;
+CREATE DATABASE movies;
+
+USE movies;
+
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(50),
+  status VARCHAR(20)
+)
